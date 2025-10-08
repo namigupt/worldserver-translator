e2403a64bdc7776dc92969b0423b78478ddfd703
Namit Gupta <namigupt@adobe.com>
2025-10-08 15:05:15 +0530

    updated

--- tools/translator/translator.js
+++ tools/translator/translator.js
@@ -3,12 +3,12 @@ import DA_SDK from 'https://da.live/nx/utils/sdk.js';
 (async function init() {
   // eslint-disable-next-line no-unused-vars
   const { context, token, actions } = await DA_SDK;
-  Object.keys(context).forEach((key) => {
+  Object.keys(actions).forEach((key) => {
     const h3 = document.createElement('h3');
     h3.textContent = `${key}`;
 
     const para = document.createElement('p');
-    para.textContent = context[key];
+    para.textContent = actions[key];
 
     document.body.append(h3, para);
   });
